26a2ab243cee272ba74122932872e039d1fae6cf
Miguel Ribeiro <miguel.js.ribeiro@outlook.com>
2026-09-25 09:49:12 +0100

    Configure trusted CSRF origins

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = os.getenv(
     ""
 ).split(",")
 
+CSRF_TRUSTED_ORIGINS = os.getenv(
+    "DJANGO_CSRF_TRUSTED_ORIGINS",
+    ""
+).split(",")
 # Application definition
 
 INSTALLED_APPS = [
